c35f0e52bf8730e91bc5cf5166cc5bcfbabbfe0b
Xapadoan <HollyolGamu@hotmail.com>
2026-09-19 07:48:26 +0200

    chore: cleaner amount recognition regexp for test

--- src/ingredient/test/amount-recognition_test.go
+++ src/ingredient/test/amount-recognition_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestAmountRecognitionRegexp(t *testing.T) {
 	for _, sample := range RealLifeSamples() {
-		regex := regexp.MustCompile(".*(" + dom.AmountRecognitionRegexp() + ") ?.*")
+		regex := regexp.MustCompile("(" + dom.AmountRecognitionRegexp() + ")")
 		matches := regex.FindStringSubmatch(sample.TestString)
 		if len(matches) < 2 && sample.ExpectedResult.Amount != 1 {
 			t.Errorf("Failed to detect any amount for sample \"%v\"", sample.TestString)
